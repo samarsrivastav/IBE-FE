09a8ad9c16e1f3834de850483e5e0a5b113a2713
samarsrivastav <samarsrivastav69@gmail.com>
2025-04-08 23:01:17 +0530

    IBE05-100[bug][very small screen fix]

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,15 +4,31 @@ import { Home } from "./Pages/Home/Home";
 import { Checkout } from "./Pages/Checkout/Checkout";
 import { Footer } from "./Component/Footer/Footer";
 import { Navbar } from "./Component/Navbar/Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DynamicBackground from "./Component/DynamicChanges/DynamicBackground";
 import { GoogleTranslateProvider } from "./Component/Translation/GoogleTranslateProvider";
 import RoomResults from "./Pages/Properties/RoomResults";
 import BookingSummaryPage from "./Pages/BookingSummaryPage/BookingSummaryPage";
+import UnsupportedScreenWidth from "./Component/UnsupportedScreenWidth/UnsupportedScreenWidth";
+import PageNotFound from "./Component/PageNotFound/PageNotFound";
 
 function AppContent() {
   const [language, setLanguage] = useState<string>("en");
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
   const location = useLocation();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (windowWidth < 352) {
+    return <UnsupportedScreenWidth />;
+  }
 
   return (
     <div className="app-container">
@@ -24,6 +40,7 @@ function AppContent() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/property" element={<RoomResults />} />
           <Route path="/confirmation-page/:bookingId" element={<BookingSummaryPage/>}/>
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
       <Footer />
